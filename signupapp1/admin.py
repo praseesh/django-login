80fd8123af6623f0e48544b751d4bd8e7fda6a32
Praseesh <praseeshprasee@gmail.com>
2024-05-22 23:45:09 +0530

    models

--- signupapp1/admin.py
+++ signupapp1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from signupapp1.models import CustomUser
 # Register your models here.
+
+
+admin.site.register(CustomUser)
--- signupapp1/models.py
+++ signupapp1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'User'
+        
+    def __str__(self):
+        return self.name
