f5f64660329b0c0d250cc6af3f0de3fba7ec0586
Praseesh <praseeshprasee@gmail.com>
2024-05-24 10:53:23 +0530

    session

--- signupapp1/views.py
+++ signupapp1/views.py
@@ -7,7 +7,9 @@ from .forms import UserLoginForm
 
 User = get_user_model()
 
-def user_login(request):    
+def user_login(request):   
+    if 'email' in request.session:
+         return redirect('home')      
     if request.method == "POST":
         form = UserLoginForm(request.POST)
         if form.is_valid():
@@ -17,18 +19,21 @@ def user_login(request):
                 user = User.objects.get(email=email)
                 if check_password(password, user.password):
                     login(request, user)
+                    request.session['email'] = email
                     return redirect('home')
                 else:
                     msg = "Invalid email or password."
             except User.DoesNotExist:
                 msg = "Invalid email or password."
-            
             return render(request, 'login.html', {'form': form, 'msg': msg})
     else:
         form = UserLoginForm()
     return render(request, 'login.html', {'form': form})
 
 def signup(request):
+    if 'username' in request.session:
+        return redirect('home')
+    
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -39,8 +44,8 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 def home(request):
-    if request.user.is_authenticated :
+    if 'email' in request.session and request.user.is_authenticated :
         return render(request, 'home.html')
-        
+    
     return redirect('user_login')
     
