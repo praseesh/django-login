0d2496aa3f6068615d3ed376317e9ba29a90e6ae
Praseesh <praseeshprasee@gmail.com>
2024-05-24 21:25:29 +0530

    admin

--- signupapp1/urls.py
+++ signupapp1/urls.py
@@ -4,5 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.user_login, name="user_login"),
     path('signup/', views.signup, name="signup"),
-    path('home', views.home, name="home" )
+    path('home', views.home, name="home" ),
+    path('logout', views.logout, name="logout"),
+    path('admin_login', views.admin_login, name="admin_login"),
+    path('admin',views.admin_home,name='admin')
 ]
--- signupapp1/views.py
+++ signupapp1/views.py
@@ -4,6 +4,7 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.hashers import check_password
 from .forms import UserLoginForm
+# from .models import CustomUser
 
 User = get_user_model()
 
@@ -31,13 +32,13 @@ def user_login(request):
     return render(request, 'login.html', {'form': form})
 
 def signup(request):
-    if 'username' in request.session:
+    if 'email' in request.session:
         return redirect('home')
     
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  
+            form.save()  
             return redirect('user_login')
     else:
         form = CustomUserCreationForm()
@@ -48,4 +49,33 @@ def home(request):
         return render(request, 'home.html')
     
     return redirect('user_login')
+
+def logout(request):
+    if 'email'in request.session:
+        request.session.flush()
+    return redirect('user_login')
+
+admin_username = 'admin'
+admin_password = '1234'
+def admin_login(request):
+    if request.method == "POST":
+        username = request.POST.get('admin1_username')
+        password = request.POST.get('admin1_password')
+        
+        if username == admin_username and password == admin_password:
+            request.session['username']=username 
+            return redirect('admin')
+        else:
+            msg = "Invalid username or password."
+            return render(request, 'adminlogin.html', {'msg': msg})
     
+    return render(request, 'adminlogin.html')
+
+def admin_home(request):
+    if 'username' in request.session:
+        return redirect('admin')
+    if request.method=='POST':
+        users = User.objects.all()
+        if users is not None:
+            return render(request, 'admin.html', {'users': users})
+        return render(request,'admin.html', {'users': 'The requested user is Empty'})
