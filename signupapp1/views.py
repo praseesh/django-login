2b34eaf1f5e9d7d828935939211708fe6ad9eb6d
Praseesh <praseeshprasee@gmail.com>
2024-06-01 14:55:01 +0530

    admin

--- signupapp1/views.py
+++ signupapp1/views.py
@@ -12,6 +12,7 @@ User = get_user_model()
 admin_username = 'admin'
 admin_password = '1234'
 
+
 def user_login(request):   
     if 'email' in request.session:
          return redirect('home')      
