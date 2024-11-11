73bc8e54e37c0c3211de466f903f135f70ab8585
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-11-11 18:27:17 +0530

    Update settings.py

--- signup/settings.py
+++ signup/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+#.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z9do3r1$=hy*-3s*#83$-ie#n#tt9=m1err_eid%8qg01&uu+a'
