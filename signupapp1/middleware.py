b8941b35f491cdb17d7410169abc338341a1e4e8
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-01-08 22:53:43 +0530

    Update middleware.py

--- signupapp1/middleware.py
+++ signupapp1/middleware.py
@@ -5,5 +5,6 @@ class NoCacheMiddleware(MiddlewareMixin):
         response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
         response['Pragma'] = 'no-cache'
         response['Expires'] = '0'
+      
         return response
     
