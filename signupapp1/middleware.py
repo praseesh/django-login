e8ca0dcc449928ce74f8835c5b93118853ecd9e1
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-01-08 22:54:08 +0530

    Update middleware.py

--- signupapp1/middleware.py
+++ signupapp1/middleware.py
@@ -1,5 +1,6 @@
 from django.utils.deprecation import MiddlewareMixin
 
+#For cache controll
 class NoCacheMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
