e3cebbbd421b1eb32bad3ead247a1a1170853fb7
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-01-18 21:08:59 +0530

    Update models.py

--- signupapp1/models.py
+++ signupapp1/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
